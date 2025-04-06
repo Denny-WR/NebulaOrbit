278167f423f13fefa6ccb9b63ec3266019af0cf8
Denny-WR <denydias96@gmail.com>
2025-04-06 11:52:32 +0000

    mudar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 function Home (){
-    return <h1> Uma coisa importante!! Nunca Desista, chega de ser fraco 🫡 </h1>
+    return <h1> Uma coisa importante!! Nunca Desista, chega de ser fraco, Naruto nunca desistiria, Gon não desisitiria, Seja Forte 🫡 </h1>
 }
 
 export default Home;  
